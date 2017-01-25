6256b7b1d9e49be74df73ab5bf88e2eaf2f8647f
Vitalii Zagorodniuk <vitaliizagorodniuk@gmail.com>
2017-01-25 15:11:20 +0200

    Update view

--- views/language/adminlte/index.php
+++ views/language/adminlte/index.php
@@ -48,6 +48,8 @@ CSS
     <div class="box-header">
         <?php if ($canAdd): ?>
             <?= Html::a(Yii::t('multilang-tools', 'Add language'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?php else: ?>
+            &nbsp;
         <?php endif; ?>
         <div class="box-tools pull-right">
             <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
